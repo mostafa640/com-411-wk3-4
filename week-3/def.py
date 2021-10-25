4db7389c34f9b7e1adfa6054a7962649c2c7d00f
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-25 01:02:35 +0100

    parameter

--- week-3/def.py
+++ week-3/def.py
@@ -1,5 +1,13 @@
-def listen():
-  print("what sound did i hear")
-  sound = input()
-  print("that was a loud", sound)
-listen()
+# def listen():
+#   print("what sound did i hear")
+#   sound = input()
+#   print("that was a loud", sound)
+# listen()
+def identity():
+  print("what they see")
+  see = input()
+  if see == "a large boulder":
+    print("its time to run")
+  else:
+    print("we will be fine")
+identity()
